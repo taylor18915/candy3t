0ff223a18b391e76819498777399438412f75c72
taylor18915 <54372304+taylor18915@users.noreply.github.com>
2019-10-24 15:00:35 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Candy3.Data;
+
+namespace Candy3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Candy3.Data.Candy> Candy { get; set; }
+        public DbSet<Candy3.Data.CandyStores> CandyStores { get; set; }
+        public DbSet<Candy3.Data.Orderscs> Orderscs { get; set; }
+    }
+}
---
+++ CandyStores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Candy3.Data
+{
+    public class CandyStores
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string CName { get; set; }
+        public string Ctype { get; set; }
+        public int CPrice { get; set; }
+        public int Csize { get; set; }
+    }
+}
