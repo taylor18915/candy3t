e873bb9ea10e1fc0515f91b0f0aa4abedf09a3d3
taylor18915 <54372304+taylor18915@users.noreply.github.com>
2019-10-24 15:03:03 -0500

    Add files via upload

---
+++ Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Candy3.Pages
+{  [Authorize(Roles="admin")]
+    public class PrivacyModel : PageModel
+    {
+        public string CurrentRole { get; set; }
+        public void OnGet()
+        {
+            CurrentRole = "SLT";
+        }
+    }
+}
